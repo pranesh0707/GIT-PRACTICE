617d87cd2bbb70f412b6c0c7bf42538db1b9da00
pranesh0707 <praneshsekar07@gmail.com>
2025-01-23 21:31:01 +0530

    update

---
+++ tourism/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  // tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
